96a5d9c1d39b84d0a33cb24134b171ab0ddef43e
trandinhnghiem2014676 <nghiemb2014676@student.ctu.edu.vn>
2024-04-02 20:08:23 +0700

    Tao trang them lien he

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="page">
+    <h4>Thêm Liên hệ</h4>
+    <ContactForm @submit:contact="addContact" />
+    <p>{{ message }}</p>
+  </div>
+</template>
+<script>
+import ContactForm from '@/components/ContactForm.vue';
+import ContactService from '@/services/contact.service';
+export default {
+  components: {
+    ContactForm,
+  },
+
+  data() {
+    return {
+      contact: null,
+      message: '',
+    };
+  },
+  methods: {
+    async addContact(data) {
+      try {
+        await ContactService.create(data);
+        this.message = 'Thêm liên hệ thành công.';
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
+</script>
